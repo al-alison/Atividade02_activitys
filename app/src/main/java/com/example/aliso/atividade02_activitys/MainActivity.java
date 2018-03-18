b96ed057ca2f55b585f3b0bedfcb7534f46d5176
Alison Almeida <alison.almeida00@gmail.com>
2018-03-18 16:15:29 -0300

    Atividade02

---
+++ app/src/main/java/com/example/aliso/atividade02_activitys/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.aliso.atividade02_activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * @author Alison Almeida - RA: 818119557 - SI3AN-MCA
+ */
+public class MainActivity extends Activity {
+
+    /**
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
